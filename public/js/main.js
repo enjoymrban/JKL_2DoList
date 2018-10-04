0ca77206d722180592c2fe0c0bac5119a2492bb9
enjoymrban <knecht.silvan@gmail.com>
2018-10-04 08:54:24 +0200

    added comments, object destructuring

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,15 @@
 $(function () {
 
+    // Fetches all task and sorts them into Todo and Done
     $.ajax({
         url: "api/tasks",
         type: "GET",
         dataType: "json"
     }).done((json) => {
         $.each(json, (key, value) => {
+            const {isDone} = value;
             let newTask = taskTemplate(value);
-            if (value.isDone) {
+            if (isDone) {
                 appendDone(newTask, value);
             } else {
                 appendToDo(newTask, value);
@@ -16,50 +18,62 @@ $(function () {
         });
     });
 
+    // Appends all sorted ToDos to the .todo Div
     function appendToDo(newTask, value) {
+        const {id} = value;
         $('.todo').append(newTask);
-        $('#task' + value.id).change(() => {
-            if ($('#task' + value.id).is(":checked")) {
+
+        // Checks when the checkbox is changed and updates the task --> sends it to Done
+        $('#task' + id).change(() => {
+            if ($('#task' + id).is(":checked")) {
                 updateTask(value);
             }
-
         });
-        $('#deleteIcon' + value.id).click(() => {
-            deleteTask(value.id);
+
+        //  Functions of the deleteIcon
+        $('#deleteIcon' + id).click(() => {
+            deleteTask(id);
         });
     }
 
+    // Appends all sorted ToDos to the .todo Div
     function appendDone(newTask, value) {
+        const {id} = value;
         $('.done').append(newTask);
-        $('#taskLabel' + value.id).css("text-decoration", "line-through");
-        $('#task' + value.id).attr("checked", true);
-        $('#task' + value.id).change(() => {
-            if (!$('#task' + value.id).is(":checked")) {
+        $('#taskLabel' + id).css("text-decoration", "line-through");
+        $('#task' + id).attr("checked", true);
+
+        // Checks when the checkbox is changed and updates the task --> sends it to ToDo
+        $('#task' + id).change(() => {
+            if (!$('#task' + id).is(":checked")) {
                 updateTask(value);
             }
-
         });
-        $('#deleteIcon' + value.id).click(() => {
-            deleteTask(value.id);
+
+        //  Functions of the deleteIcon
+        $('#deleteIcon' + id).click(() => {
+            deleteTask(id);
         });
     }
 
 
+    // Updates the task, removes it from its current list and appends it to the new list
     function updateTask(value) {
+        const {id, description, isDone} = value;
         $.ajax({
-            url: 'api/tasks/' + value.id,
+            url: 'api/tasks/' + id,
             dataType: 'json',
             type: 'put',
             contentType: 'application/json',
-            data: JSON.stringify({ "description": value.description, "isDone": !value.isDone }),
+            data: JSON.stringify({ "description": description, "isDone": !isDone }), //isDone gets inverted in every update, maybe change this!
             processData: false,
             success: function (data, textStatus, jQxhr) {
+                const {id, isDone} = data;
                 let newTask = taskTemplate(data);
-                $('#taskDiv' + data.id).remove();
-                if (data.isDone) {
+                $('#taskDiv' + id).remove();
+                if (isDone) {
                     appendDone(newTask, data);
                 } else {
-
                     appendToDo(newTask, data);
                 }
                 // location.reload();
@@ -68,9 +82,9 @@ $(function () {
                 console.log(errorThrown);
             }
         });
-
     }
 
+    // Delete a Task request
     function deleteTask(id) {
         $.ajax({
             url: "api/tasks/" + id,
@@ -103,9 +117,11 @@ $(function () {
                 console.log(request.responseText);
             }
         });
+        // Empties the input field!
         $('#taskDescription').val('');
     });
 
+    // Showes and hids the done DIV when switching the slider in the nav
     $('#showDoneC').change(() => {
         if ($('#showDoneC').is(":checked")) {
             console.log("showDone");
@@ -116,19 +132,20 @@ $(function () {
 
     });
 
-
+    // Templeta to create a new Task including text and icons
     function taskTemplate(value) {
+        const {id, description} =value;
         let newTask =
-            `<div class="task form-check" id="taskDiv${value.id}">
+            `<div class="task form-check" id="taskDiv${id}">
                 <div class="pretty p-icon p-jelly p-round maginFix">
-                    <input type="checkbox" id="task${value.id}">
+                    <input type="checkbox" id="task${id}">
                     <div class="state p-info taskSettings">
                         <i class="icon material-icons">done</i>
-                        <label id="taskLabel${value.id}">${value.description}</label>            
+                        <label id="taskLabel${id}">${description}</label>
                     </div>
-                </div> 
+                </div>
                 <div class="taskSettings">
-                    <i id="deleteIcon${value.id}" class='material-icons'>delete</i>
+                    <i id="deleteIcon${id}" class='material-icons'>delete</i>
                 </div>
             </div>`;
         return newTask;
