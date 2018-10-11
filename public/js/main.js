5e06d050e9afcc4b1c2dab3857d5b97b5868b700
enjoymrban <knecht.silvan@gmail.com>
2018-10-11 22:27:22 +0200

    cleaned code

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,4 @@
 $(function () {
-
     // Fetches all task and sorts them into Todo and Done
     $.ajax({
         url: "api/tasks",
@@ -9,48 +8,36 @@ $(function () {
         // Prints a table with all tasks to the console
         console.table(json);
         $.each(json, (key, value) => {
-            const {isDone} = value;
-            let newTask = taskTemplate(value);
-            if (isDone) {
-                appendDone(newTask, value);
-            } else {
-                appendToDo(newTask, value);
-            }
-
+            appendTask(value);
         });
     });
+
 });
 
-// Appends all sorted ToDos to the .todo Div
-function appendToDo(newTask, value) {
-    const {id} = value;
-    $('.todo').append(newTask);
+// append Task to the right Table
+function appendTask(value) {
+    let newTask = taskTemplate(value);
+    const {
+        id,
+        isDone
+    } = value;
+    let divClass = "";
+    if (isDone) {
+        divClass = ".done";
+        $(divClass).append(newTask);
+        $('#taskLabel' + id).css("text-decoration", "line-through");
+        $('#task' + id).attr("checked", true);
 
-    // Checks when the checkbox is changed and updates the task --> sends it to Done
-    $('#task' + id).change(() => {
-        if ($('#task' + id).is(":checked")) {
-            changeTaskIsDone(value);
-        }
-    });
+    } else {
+        divClass = ".todo";
+        $(divClass).append(newTask);
+    }
 
-    //  Functions of the deleteIcon
-    $('#deleteIcon' + id).click(() => {
-        deleteTask(id);
-    });
-}
-
-// Appends all sorted ToDos to the .todo Div
-function appendDone(newTask, value) {
-    const {id} = value;
-    $('.done').append(newTask);
-    $('#taskLabel' + id).css("text-decoration", "line-through");
-    $('#task' + id).attr("checked", true);
 
     // Checks when the checkbox is changed and updates the task --> sends it to ToDo
     $('#task' + id).change(() => {
-        if (!$('#task' + id).is(":checked")) {
-            changeTaskIsDone(value);
-        }
+        changeTaskIsDone(value);
+
     });
 
     //  Functions of the deleteIcon
@@ -59,86 +46,12 @@ function appendDone(newTask, value) {
     });
 }
 
-
-// Updates the task, removes it from its current list and appends it to the new list
-function changeTaskIsDone(value) {
-    const {id, description, isDone} = value;
-    $.ajax({
-        url: 'api/tasks/' + id,
-        dataType: 'json',
-        type: 'put',
-        contentType: 'application/json',
-        data: JSON.stringify({ "description": description, "isDone": !isDone }), //isDone gets inverted in every update, maybe change this!
-        processData: false,
-        success: function (data, textStatus, jQxhr) {
-            const {id, isDone} = data;
-            let newTask = taskTemplate(data);
-            $('#taskDiv' + id).remove();
-            if (isDone) {
-                appendDone(newTask, data);
-            } else {
-                appendToDo(newTask, data);
-            }
-            // location.reload();
-        },
-        error: function (jqXhr, textStatus, errorThrown) {
-            console.log(errorThrown);
-        }
-    });
-}
-
-// Delete a Task request
-function deleteTask(id) {
-    $.ajax({
-        url: "api/tasks/" + id,
-        type: "DELETE",
-        dataType: "json",
-        success: function (data) {
-            const {id} = data;
-            $('#taskDiv' + id).remove();
-        },
-        error: function (errorThrown) {
-            console.log(errorThrown);
-        }
-
-    });
-}
-
-// Actionlistener to creat a new Task
-$('#createTaskF').submit(() => {
-    $.ajax({
-        url: 'api/tasks',
-        dataType: 'json',
-        type: 'post',
-        contentType: 'application/json',
-        data: JSON.stringify({ "description": $('#taskDescription').val(), "isDone": false }),
-        processData: false,
-        success: function (data, textStatus, jQxhr) {
-            let newTask = taskTemplate(data);
-            appendToDo(newTask, data);
-        },
-        error: function (request, textStatus, error) {
-            console.log(request.responseText);
-        }
-    });
-    // Empties the input field!
-    $('#taskDescription').val('');
-});
-
-// Showes and hids the done DIV when switching the slider in the nav
-$('#showDoneC').change(() => {
-    if ($('#showDoneC').is(":checked")) {
-        console.log("showDone");
-        $('#doneContainer').show();
-    } else {
-        $('#doneContainer').hide();
-    }
-
-});
-
 // Templeta to create a new Task including text and icons
 function taskTemplate(value) {
-    const {id, description} =value;
+    const {
+        id,
+        description
+    } = value;
     let newTask =
         `<div class="task form-check" id="taskDiv${id}">
             <div class="pretty p-icon p-jelly p-round maginFix">
@@ -154,3 +67,86 @@ function taskTemplate(value) {
         </div>`;
     return newTask;
 }
+
+// Updates the task, removes it from its current list and appends it to the new list
+function changeTaskIsDone(value) {
+    const {
+        id,
+        description,
+        isDone
+    } = value;
+
+    $.ajax({
+        url: 'api/tasks/' + id,
+        dataType: 'json',
+        type: 'put',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "description": description,
+            "isDone": !isDone
+        }), //isDone gets inverted in every update, maybe change this!
+        processData: false,
+        success: function (data, textStatus, jQxhr) {
+            const {
+                id
+            } = data;
+            $('#taskDiv' + id).remove();
+            appendTask(data);
+
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+    });
+}
+
+// Delete a Task request
+function deleteTask(id) {
+    $.ajax({
+        url: "api/tasks/" + id,
+        type: "DELETE",
+        dataType: "json",
+        success: function (data) {
+            const {
+                id
+            } = data;
+            $('#taskDiv' + id).remove();
+        },
+        error: function (errorThrown) {
+            console.log(errorThrown);
+        }
+    });
+}
+
+// Actionlistener to creat a new Task
+$('#createTaskF').submit(() => {
+    $.ajax({
+        url: 'api/tasks',
+        dataType: 'json',
+        type: 'post',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "description": $('#taskDescription').val(),
+            "isDone": false
+        }),
+        processData: false,
+        success: function (data, textStatus, jQxhr) {
+            appendTask(data);
+        },
+        error: function (request, textStatus, error) {
+            console.log(request.responseText);
+        }
+    });
+    // Empties the input field!
+    $('#taskDescription').val('');
+});
+
+// Showes and hids the done DIV when switching the slider in the nav
+$('#showDoneC').change(() => {
+    if ($('#showDoneC').is(":checked")) {
+        $('#doneContainer').show();
+    } else {
+        $('#doneContainer').hide();
+    }
+
+});
