f85745f609e64fe8828588a6be88f59de14dd229
silviojaeger <silvio.jaeger@hotmail.com>
2018-11-23 14:08:10 +0100

    cache

--- public/js/sw.js
+++ public/js/sw.js
@@ -1,1 +1,11 @@
 console.log("sw.js");
+
+var cacheName = 'todoList';
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => cache.addAll([
+                '../../'
+            ]))
+    );
+});
